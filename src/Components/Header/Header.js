f63fcf566025b351a15117ccd4b1219c6ded7543
Burma <99819603+Nuradilova@users.noreply.github.com>
2022-12-09 13:11:09 +0600

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import kroha from '../images/kroha.png'
+import bag from '../images/bag.png'
+import './Header.css'
+import { NavLink } from 'react-router-dom';
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
-}
+
+
+const Header = () => {
+    return (
+        <>
+        <div className='main-header'>
+            <div className='header'>
+                <NavLink exact to='/oplata'><p>Оплата и доставка</p></NavLink>
+                <NavLink exact to='/otzyv'><p>Отзывы</p></NavLink>
+                <NavLink exact to='/'><img className='kroha' src={kroha}/></NavLink>
+            </div>
+            <div className='bagDiv'>
+                <NavLink exact to='/bag'><img className='bag' src={bag}/></NavLink>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Header;
